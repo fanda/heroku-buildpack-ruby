2bf2ce45cb4628ab6344c77482849d57de7b2430
fanda <fandisek@gmail.com>
2019-07-27 01:13:27 +0200

    be master

--- lib/language_pack/base.rb
+++ lib/language_pack/base.rb
@@ -7,7 +7,7 @@ Encoding.default_external = Encoding::UTF_8 if defined?(Encoding)
 
 # abstract class that all the Ruby based Language Packs inherit from
 class LanguagePack::Base
-  VENDOR_URL = "vendor"
+  VENDOR_URL = "file://#{File.dirname(__FILE__)}../../vendor"
 
   attr_reader :build_path, :cache_path
 
@@ -221,4 +221,6 @@ private ##################################
     FileUtils.mkdir_p File.dirname(to)
     system("cp -a #{from}/. #{to}")
   end
+
 end
+
--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -168,7 +168,8 @@ ERROR
 
     FileUtils.mkdir_p(slug_vendor_ruby)
     Dir.chdir(slug_vendor_ruby) do
-      run("tar xvfj #{File.dirname(__FILE__)}/../../#{VENDOR_URL}/#{ruby_vm}-#{ruby_version}.#{RUBY_PKG_EXTENSION}")
+      puts "Get Ruby in: #{VENDOR_URL}"
+      run("curl #{VENDOR_URL}/#{ruby_vm}-#{ruby_version}.#{RUBY_PKG_EXTENSION}  -s -o - | tar xvfj -")
     end
     error invalid_ruby_version_message unless $?.success?
 
