1e5c9060db6f95efd6f9040d8fb8f9e86e32ac80
fanda <fandisek@gmail.com>
2019-07-29 09:07:22 +0200

    be master

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -405,11 +405,12 @@ params = CGI.parse(uri.query || "")
   # add bundler to the load path
   # @note it sets a flag, so the path can only be loaded once
   def add_bundler_to_load_path
-    @bundler_loadpath ||= begin
+    return if @bundler_loadpath
+
                             sleep
-      $: << File.expand_path(Dir["#{slug_vendor_base}/gems/bundler*/lib"].first)
-      true
-    end
+
+    $: << File.expand_path(Dir["#{slug_vendor_base}/gems/bundler*/lib"].first)
+    @bundler_loadpath =  true
   end
 
   # detects if a gem is in the bundle.
